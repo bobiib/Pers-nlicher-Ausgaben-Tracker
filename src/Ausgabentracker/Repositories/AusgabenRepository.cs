ebe5a07adff7ecf221baee03869aedf09afe4ca9
Boris <boris_tomovski@sluz.ch>
2026-04-02 09:26:20 +0200

    Create AusgabenRepository.cs

---
+++ src/Ausgabentracker/Repositories/AusgabenRepository.cs
@@ -0,0 +1,54 @@
+using AusgabenTracker.Database;
+using AusgabenTracker.Models;
+using Microsoft.Data.Sqlite;
+using System;
+using System.Collections.Generic;
+
+namespace AusgabenTracker.Repositories
+{
+    public class AusgabenRepository
+    {
+        // 1. CREATE: Eine neue Ausgabe speichern
+        public void SpeichereAusgabe(Ausgabe neueAusgabe)
+        {
+            string sql = $@"
+                INSERT INTO Transaktion (Betrag, Datum, Notiz, KategorieId, Typ, SpezialFeld) 
+                VALUES ({neueAusgabe.Betrag.ToString(System.Globalization.CultureInfo.InvariantCulture)}, 
+                        '{neueAusgabe.Datum:yyyy-MM-dd HH:mm:ss}', 
+                        '{neueAusgabe.Notiz}', 
+                        {neueAusgabe.KategorieId}, 
+                        'Ausgabe', 
+                        '{neueAusgabe.IstSteuerlichAbsetzbar}')";
+
+            DatabaseManager.Instance.ExecuteNonQuery(sql);
+        }
+
+        // 2. READ: Alle Kategorien aus der DB laden (für ein Dropdown in der UI)
+        public List<Kategorie> LadeAlleKategorien()
+        {
+            var kategorien = new List<Kategorie>();
+            string connectionString = "Data Source=Ausgaben.db"; // Pfad zur DB
+
+            using (var connection = new SqliteConnection(connectionString))
+            {
+                connection.Open();
+                var command = connection.CreateCommand();
+                command.CommandText = "SELECT Id, Name, Beschreibung FROM Kategorie";
+
+                using (var reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        kategorien.Add(new Kategorie
+                        {
+                            Id = reader.GetInt32(0),
+                            Name = reader.GetString(1),
+                            Beschreibung = reader.IsDBNull(2) ? "" : reader.GetString(2)
+                        });
+                    }
+                }
+            }
+            return kategorien;
+        }
+    }
+}
